2c4ee8b1f3839719fb1249266b225b08d130e662
lyl <lylmmkj2019>
2019-11-14 18:03:43 +0800

    剑指offer No.35 第一个不重复的字符

---
+++ sword_offer/src/NO_35_FirstNotRepeatChar.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class NO_35_FirstNotRepeatChar {
+    public static int solution(String str) {
+        if(str == null || "".equals(str)) {
+            return -1;
+        }
+
+        int[] arr = new int[256];
+        for(int i = 0; i < str.length(); i ++) {
+            arr[str.charAt(i)] += 1;
+        }
+
+        for(int i = 0; i < str.length(); i ++) {
+            if(arr[str.charAt(i)] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+    }
+}
