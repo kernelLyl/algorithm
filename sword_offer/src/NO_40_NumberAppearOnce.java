6bc4c1e0d9873946c193532c9acef3d95f22fbbb
lyl <lylmmkj2019>
2019-11-26 11:24:23 +0800

    剑指offer No.40 只出现一次的数 添加注释

--- sword_offer/src/NO_40_NumberAppearOnce.java
+++ sword_offer/src/NO_40_NumberAppearOnce.java
@@ -1,5 +1,5 @@
 public class NO_40_NumberAppearOnce {
-    public void FindNumsAppearOnce(int [] array,int num1[] , int num2[]) {
+    public static void FindNumsAppearOnce(int [] array,int num1[] , int num2[]) {
         num1[0] = 0;
         num2[0] = 0;
         if(array.length == 0)
@@ -22,8 +22,14 @@ public class NO_40_NumberAppearOnce {
         }
     }
 
-    public boolean isa1(int i, int index){
+    //判断i 的第index位是否为1
+    public static boolean isa1(int i, int index){
         i = i >> index;
         return (i & 1) == 1;
+    }
+
+    public static void main(String[] args) {
+        int[] test = {1, 2, 3 , 4, 10, 205, 34410, 205, 24410, 2, 1, 3};
+        FindNumsAppearOnce(test, new int[1], new int[1]);
     }
 }
