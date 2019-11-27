8b4dd7202119c785046aa25cf7b7e9ac7e0e64fe
kernelLyl <kernel.lyl@gmail.com>
2019-11-27 22:12:21 +0800

    NO_44 扑克牌的顺子fix

---
+++ sword_offer/src/NO_44_ContinuousCards.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class NO_44_ContinuousCards {
+    public boolean isContinuous(int[] numbers) {
+        int numOfZero = 0;
+        int numOfInterval = 0;
+        int length = numbers.length;
+        if(length == 0){
+            return false;
+        }
+        Arrays.sort(numbers);
+        for (int i = 0; i < length - 1; i++) {
+            // 计算癞子数量
+            if (numbers[i] == 0) {
+                numOfZero++;
+                continue;
+            }
+            // 对子，直接返回
+            if (numbers[i] == numbers[i + 1]) {
+                return false;
+            }
+            numOfInterval += numbers[i + 1] - numbers[i] - 1;
+        }
+        if (numOfZero >= numOfInterval) {
+            return true;
+        }
+        return false;
+    }
+}
