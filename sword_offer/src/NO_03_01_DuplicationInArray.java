916b588c78732c642f800ba5c2647c417be27ed1
kernelLyl <kernel.lyl@gmail.com>
2019-12-03 22:40:43 +0800

    NO_03_01 数组中重复的数字

---
+++ sword_offer/src/NO_03_01_DuplicationInArray.java
@@ -0,0 +1,30 @@
+public class NO_03_01_DuplicationInArray {
+    // Parameters:
+    //    numbers:     an array of integers
+    //    length:      the length of array numbers
+    //    duplication: (Output) the duplicated number in the array number,length of duplication array is 1,so using duplication[0] = ? in implementation;
+    //                  Here duplication like pointor in C/C++, duplication[0] equal *duplication in C/C++
+    //    这里要特别注意~返回任意重复的一个，赋值duplication[0]
+    // Return value:       true if the input is valid, and there are some duplications in the array number
+    //                     otherwise false
+    public boolean duplicate(int numbers[],int length,int [] duplication) {
+        if(numbers == null || numbers.length == 0) {
+            return false;
+        }
+
+        for(int i = 0; i < length; i ++) {
+
+            while (numbers[i] != i) {
+                int m = numbers[i];
+                if (numbers[m] == m) {
+                    duplication[0] = m;
+                    return true;
+                } else {
+                    numbers[i] = numbers[m];
+                    numbers[m] = m;
+                }
+            }
+        }
+        return false;
+    }
+}
