12c3b6e207ff878ec935eace6f3befd710329c61
lyl <lylmmkj2019>
2019-12-05 19:52:31 +0800

    fix

---
+++ sword_offer/src/NO_06_PrintListInReverseOrder.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+
+//从尾到头打印链表
+public class NO_06_PrintListInReverseOrder {
+    public ArrayList<Integer> printListFromTailToHead(NO_37_FirstCommonNode.ListNode listNode) {
+        Deque<Integer> deque = new LinkedList<>();
+    }
+}
