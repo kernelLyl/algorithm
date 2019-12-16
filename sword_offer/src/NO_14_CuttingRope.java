2f15058ac4e5edd7fe195e1af58fb237cddeee55
kernelLyl <kernel.lyl@gmail.com>
2019-12-16 22:20:44 +0800

    NO_14 剪绳子

--- sword_offer/src/NO_14_CuttingRope.java
+++ sword_offer/src/NO_14_CuttingRope.java
@@ -1,11 +1,29 @@
 public class NO_14_CuttingRope {
-    public int cutRope(int target) {
-        int[] value = new int[60];
-        value[1] = 1;
-        value[2] = 1;
-        for(int i = 3; i <= n; i ++) {
-
+    public static int cutRope(int target) {
+        if (target == 2) {
+            return 1;
         }
-        return 0;
+        if (target == 3) {
+            return 2;
+        }
+        int[] value = new int[61];
+        value[1] = 1;
+        value[2] = 2;
+        value[3] = 3;
+        for(int i = 4; i <= target; i ++) {
+            int max = 0, product = 0;
+            for(int j = 1; j < i; j ++) {
+                product = value[j] * value[i - j];
+                if(product > max) {
+                    max = product;
+                }
+            }
+            value[i] = max;
+        }
+        return value[target];
+    }
+
+    public static void main(String[] args) {
+
     }
 }
