81f570a3829ee6b981a0145a050b527cdc38b1e0
lyl <lylmmkj2019>
2019-12-16 18:53:49 +0800

    test

---
+++ sword_offer/src/NO_14_CuttingRope.java
@@ -0,0 +1,11 @@
+public class NO_14_CuttingRope {
+    public int cutRope(int target) {
+        int[] value = new int[60];
+        value[1] = 1;
+        value[2] = 1;
+        for(int i = 3; i <= n; i ++) {
+
+        }
+        return 0;
+    }
+}
