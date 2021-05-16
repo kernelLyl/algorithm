e3d42dda26cb5d33ad976b5a7ebbd38ad415eda3
liuyunlong <yunlong.liu@2339.com>
2021-05-16 13:40:07 +0800

    NO_53_最大子序和

--- company/bytedance/NO_3_LengthOfLongestSubstring.java
+++ company/bytedance/NO_3_LengthOfLongestSubstring.java
@@ -1,22 +1,23 @@
 import java.io.PrintStream;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class NO_3_LengthOfLongestSubstring {
     public static void main(String[] args) {
         String s = "abcabcbb";
-        int ans = 0;
-        Map<Character, Integer> map = new HashMap<>();
-        for (int start = 0, end = 0; end < s.length(); end ++) {
-            Character c = s.charAt(end);
-            if (map.containsKey(c)) {
-                start = Math.max(start, map.get(c));
+            int ans = 0;
+            Map<Character, Integer> map = new HashMap<>();
+            int start = 0;
+            for (int i = 0; i < s.length(); i ++) {
+                char c = s.charAt(i);
+                if (map.containsKey(c)) {
+                    start = Math.max(start, map.get(c));
+                }
+                map.put(c, i);
+                int length = i - start + 1;
+                ans = Math.max(ans, length);
 
             }
-            int length = end - start + 1;
-            ans = Math.max(ans, length);
-            map.put(c, end + 1);
-        }
+
         System.out.println(ans);
     }
 }
